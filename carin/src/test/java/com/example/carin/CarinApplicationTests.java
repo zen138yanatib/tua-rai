7309e9b2bc082860a1a0dda211739fc6f439020d
Panthonf <panthon_kansap@cmu.ac.th>
2022-02-14 22:15:03 +0700

    Update install Spring

---
+++ carin/src/test/java/com/example/carin/CarinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
