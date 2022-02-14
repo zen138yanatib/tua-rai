a37294a7fb32d8059a77a4d8d0eb1d057c3472a0
Panthonf <panthon_kansap@cmu.ac.th>
2022-02-14 22:26:16 +0700

    Update install Spring

--- carin/src/main/java/com/example/carin/CarinApplication.java
+++ carin/src/main/java/com/example/carin/CarinApplication.java
@@ -2,6 +2,8 @@ package com.example.carin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class CarinApplication {
@@ -9,5 +11,6 @@ public class CarinApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CarinApplication.class, args);
 	}
+
 
 }
